54612189f6697c14fb5d9363ca4671fa64f49c85
Markus Witt <feuerrot@feuerrot.org>
2017-08-25 00:43:26 +0200

    main: syntax fix

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import paho.mqtt.client as mqtt_client
 import graphitesend
 
 config = {
-	'mqtt_server': 'ws://miner.pr0gramm.com',
-	'mqtt_port': '8044',
+	'mqtt_server': '172.23.148.3',
+	'mqtt_port': 1883,
 	'mqtt_subscriptions': [
 		('sensor/temperature/room', 1),
 	],
@@ -23,6 +23,6 @@ if __name__ == '__main__':
 	mqtt = mqtt_client.Client()
 	mqtt.on_connect = mqtt_connect
 	mqtt.on_message = mqtt_message
-	mqtt.connect(config['mqtt_server'], config['mqtt_port']
+	mqtt.connect(config['mqtt_server'], config['mqtt_port'])
 
-	client.loop_forever()
+	mqtt.loop_forever()
